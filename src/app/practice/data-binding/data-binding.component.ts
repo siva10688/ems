e94c91589727c27ff856d2bd4b4a61706121cdc6
Siva Sankara Medisetty <SMedisetty@agility.com>
2022-12-24 17:36:01 +0530

    binding added

---
+++ src/app/practice/data-binding/data-binding.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  // template:`<h1>Hello</h1>`,
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+  name: string = 'Welcome to Binding';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
