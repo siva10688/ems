a8b2d619b25211dafcc62f4eb8bcfaf7fc47ff6b
Siva Sankara Medisetty <SMedisetty@agility.com>
2023-01-02 13:42:44 +0530

    built in directives

--- src/app/practice/directives/test-directives/test-directives.component.ts
+++ src/app/practice/directives/test-directives/test-directives.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-directives.component.css']
 })
 export class TestDirectivesComponent implements OnInit {
-
+  className: string = 'txt-color-red';
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.greenStyle);
   }
 
+  getClass(): string {
+    return 'txt-color-red';
+  }
+
+  add() {
+
+  }
+
+  delete() {
+
+  }
+
+  get greenStyle() {
+    return { 'color': 'green' };
+  }
+
+  get show() {
+    return false;
+  }
 }
