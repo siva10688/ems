2e6fb095dc785b51f6c8c954a1e3e8f38bf44ab7
Siva Sankara Medisetty <SMedisetty@agility.com>
2023-01-03 19:28:38 +0530

    folder stucture changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './practice/data-binding/data-binding.component';
@@ -10,9 +9,9 @@ import { CustomLowerCasePipe } from './practice/pipes/custom-lower.pipe';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeDetailsComponent } from './employee/employee-details/employee-details.component';
 import { TestDirectivesComponent } from './practice/directives/test-directives/test-directives.component';
-import { StudentComponent } from './student/student/student.component';
-import { StudentListComponent } from './student/student/student-list/student-list.component';
-import { StudentDetailsComponent } from './student/student/student-details/student-details.component';
+import { StudentComponent } from './student/student.component';
+import { StudentListComponent } from './student/student-list/student-list.component';
+import { StudentDetailsComponent } from './student/student-details/student-details.component';
 
 @NgModule({
   declarations: [
